d35ec92219868b597420b539cd54f0260444a4e8
Hector Olea <imacako@gmail.com>
2022-11-27 18:24:35 -0600

    fixed test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('renders learn react link', () => {
+test('renders hectorolea.dev footer', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/hectorolea.dev/i);
   expect(linkElement).toBeInTheDocument();
 });
