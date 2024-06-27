93b6467e7e620d466a4faba715efca54aa62ec41
Hector Olea <imacako@gmail.com>
2024-06-27 10:13:21 -0600

    modified about me section

--- src/portfolio/components/AboutSection.tsx
+++ src/portfolio/components/AboutSection.tsx
@@ -2,7 +2,7 @@ export const AboutSection = () => {
   return (
     <section>
       <p className='lead opacity-75 my-4'>
-        I&#x27;m a full stack developer with a DevOps mindset. I love learning
+        I&#x27;m a full stack developer with a DevOps mindset. I enjoy learning
         about new technologies, programming languages, and frameworks.
       </p>
     </section>
